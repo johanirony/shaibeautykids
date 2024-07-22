8305cd963168d7d3b5e9e482e1d1f1a8cd5722b5
Rameez <shaikhrameez516@gmail.com>
2024-07-22 17:30:37 +0530

    adding files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="w-full h-full bg-inherit">
         <h1 className="text-5xl font-semibold text-[#47312E] ">Created Just <br></br> for <span className="text-[#6C30C7]">K</span><span className="text-[#B44301]">I</span><span className="text-[#11704B]">D</span><span className="text-[#1245D5]">S</span></h1>
         <p className='text-2xl text-[#47312e] pt-3'>
-        Introducing Shai Beauty Kids! Created with a scientific understanding of developing kid skin and hair, with self-care products that prepare them for their widening world. And as kids gain independence, they also gain confidence knowing they can take care of themselves
+        Introducing Shai Beauty Kids Created with a scientific understanding of developing kid skin and hair, with self-care products that prepare them for their widening world. And as kids gain independence, they also gain confidence knowing they can take care of themselves
         </p>
        
         
